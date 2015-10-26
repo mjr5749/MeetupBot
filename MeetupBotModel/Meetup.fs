8a5b1a9a71e29421399fe03a718155b9db03d773
Michael Richards <mjr5749@gmail.com>
2015-10-25 21:47:09 -0400

    Refactored to create the ProposedMeetup module. Added scripts for launching into FSI and a basic API example. Added changeParticipantRole function.

--- MeetupBotModel/Meetup.fs
+++ MeetupBotModel/Meetup.fs
@@ -1,4 +1,4 @@
-﻿module MeetupBot.Meetup 
+﻿namespace MeetupBot.Meetup 
 
 open System
 
@@ -29,70 +29,3 @@ type Meetup =
     | ProposedMeetup of ProposedMeetupInfo
     | ScheduledMeetup
 
-let proposeMeetup1 description location (targetDates: DateRange) =
-    match targetDates with
-    | (first, last) when first.Date < DateTime.Now.Date -> None
-    | (first, last) when first.Date > last.Date -> None
-    | (first, last) ->
-        Some { 
-            Description=description; 
-            Location=location;
-            TargetDates=(first.Date, last.Date); 
-            Participants=[]
-        }
-
-let makeValidTargetDates (first:DateTime, last:DateTime) =
-    let today = DateTime.Now.Date
-    let oneYearFromToday = today.AddDays(365.0).Date
-
-    let first = max today first
-    let first = min first oneYearFromToday
-  
-    let last = max first last 
-    let last = min last (first.AddDays(365.0))
-
-    (first.Date, last.Date)
-
-
-let proposeMeetup2 description location (targetDates: DateRange) =
-    { 
-        Description=description; 
-        TargetDates=makeValidTargetDates targetDates; 
-        Location=location; 
-        Participants=[]
-    }
-
-let private createParticipant username =
-    {
-        Username = username;
-        Role = Attendee;
-        AvailableDates = [];
-        UnavailableDates = [];
-    }
-
-let private removeParticipantInternal
-    username (participants: ParticipantSchedulingInfo list) =
-    participants
-    |> List.filter (fun p -> p.Username <> username)
-
-let private updateParticipant participant meetup =
-    { meetup with
-        Participants = participant ::
-                       (removeParticipantInternal participant.Username meetup.Participants) 
-    }
-
-let getParticipant username meetup =
-    meetup.Participants
-    |> List.tryFind (fun p -> p.Username = username)
-
-let addParticipant username meetup = 
-    match getParticipant username meetup with
-    | Some participant -> meetup
-    | None -> { meetup with
-                    Participants = (createParticipant username) :: meetup.Participants
-              }
-
-let removeParticipant username meetup =
-    { meetup with
-        Participants = removeParticipantInternal username meetup.Participants
-    }
---
+++ MeetupBotModel/Scripts/example.fsx
@@ -0,0 +1,15 @@
+﻿#I __SOURCE_DIRECTORY__
+#load "load-references.fsx"
+#load "../AssemblyInfo.fs"
+      "../Meetup.fs"
+      "../ProposedMeetup.fs"
+
+open System;
+open MeetupBot.Meetup;
+open MeetupBot.Meetup.ProposedMeetup;
+
+let meetup = proposeMeetup "F# Meetup" "Rochester, NY" 
+                           (DateTime.Now, DateTime.Now.AddDays(30.0))
+             |> addParticipant (Username "mrichards")
+             |> changeParticipantRole (Username "mrichards") Presenter
+

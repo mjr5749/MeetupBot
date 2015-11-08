2f80791de08bb9ba9e3e2e29830b9204b17b54e4
Michael Richards <mjr5749@gmail.com>
2015-11-08 16:13:06 -0500

    More test coverage in the FsUnit tests

--- MeetupBotModel/ProposedMeetupTests.fs
+++ MeetupBotModel/ProposedMeetupTests.fs
@@ -15,11 +15,19 @@ type BasicProposedMeetupTests() =
         proposeMeetup "F# Meetup" "Rochester, NY" (DateTime.Now, DateTime.Now.AddDays(30.0))
         |> addParticipant x.mrichards
         |> changeParticipantRole x.mrichards Presenter
-        |> addAvailableDate x.mrichards (DateTime.Parse("2015-11-04"))
+        |> addAvailableDate x.mrichards (DateTime.Now.AddDays(1.0).Date)
 
     [<Test>]
-    member x.``meetup has one participant`` () = 
-        x.CreateBasicMeetup().Participants.Length |> should equal 1
+    member x.``meetup has one participant that is the presenter`` () = 
+        let meetup = x.CreateBasicMeetup()
+        meetup.Participants.Length |> should equal 1
+        
+        let participant = 
+            meetup
+            |> getParticipant x.mrichards |> Option.get
+
+        participant.Role |> should equal Presenter
+        participant.AvailableDates.Length |> should equal 1
 
     [<Test>]
     member x.``participant has one available date`` () =
@@ -52,8 +60,7 @@ type BasicProposedMeetupTests() =
             |> getParticipant x.mrichards
             |> Option.get
 
-        participant.UnavailableDates.Length
-        |> should equal 1
+        participant.UnavailableDates.Length |> should equal 1
 
     [<Test>]
     member x.``adding a date outside of the target date range does nothing`` () =
@@ -65,9 +72,33 @@ type BasicProposedMeetupTests() =
             |> getParticipant x.mrichards
             |> Option.get
 
-        participant.AvailableDates.Length
-        |> should equal 1
-
-        participant.UnavailableDates.Length
-        |> should equal 0
+        participant.AvailableDates.Length |> should equal 1
+        participant.UnavailableDates.Length |> should equal 0
                       
+    [<Test>]
+    member x.``participant default role is Attendee`` () =
+        let user = (Username "testuser")
+        let participant =
+            x.CreateBasicMeetup()
+            |> addParticipant user
+            |> getParticipant user
+            |> Option.get
+
+        participant.Role |> should equal Attendee
+
+    [<Test>]
+    member x.``participant can change role`` () =
+        let participant =
+            x.CreateBasicMeetup()
+            |> changeParticipantRole x.mrichards Attendee
+            |> getParticipant x.mrichards |> Option.get
+
+        participant.Role |> should equal Attendee
+
+    [<Test>]
+    member x.``all participants can be removed`` () =
+        let meetup =
+            x.CreateBasicMeetup()
+            |> removeParticipant x.mrichards
+            
+        meetup.Participants |> should equal []
